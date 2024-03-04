abaf0f5d3dd4a358b29f7618013f5a092e1a6578
rhian9843 <rihan.manwani@gmail.com>
2024-03-05 00:58:08 +0530

    Third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Webhook sucessfully enabled\n";
+  cout << "Webhook successfully enabled\n";
+  cout << "Pipeline Success\n";
   return 0;
 }
